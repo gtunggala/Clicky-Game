ea741fd3e9d608208d8a163d42d032f648bf41fd
Gabriel Tunggala <gtunggala@gmail.com>
2019-07-21 15:00:09 -0400

    components wip

---
+++ clicky-game/src/components/Game/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import "./App.css";
+const photos = require("../../data.json");
+
+export default function index() {
+  return (
+    <div>
+      <img src={"/assets/images/andy.png"} />;
+      <img src={"/assets/images/anne.png"} />;
+      <img src={"/assets/images/april.png"} />;
+      <img src={"/assets/images/ben.png"} />;
+      <img src={"/assets/images/chris.png"} />;
+      <img src={"/assets/images/donna.png"} />;
+      <img src={"/assets/images/jeanraphio.png"} />;
+      <img src={"/assets/images/jerry.png"} />;
+      <img src={"/assets/images/leslie.png"} />;
+      <img src={"/assets/images/lilsebastian.png"} />;
+      <img src={"/assets/images/ron.png"} />;
+      <img src={"/assets/images/tom.png"} />;
+    </div>
+  );
+}
